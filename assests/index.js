0b9f99f93acb70e3edca8afc0b9c6325e46ecfe7
dattu9133 <dattasaimoganti@gmail.com>
2024-01-04 16:24:29 +0530

    first commit

--- assests/index.js
+++ assests/index.js
@@ -6,16 +6,6 @@ let score = JSON.parse(localStorage.getItem('score')) || {
 
 updateScoreElement();
 
-/*
-if (!score) {
-  score = {
-    wins: 0,
-    losses: 0,
-    ties: 0
-  };
-}
-*/
-
 function playGame(playerMove) {
   const computerMove = pickComputerMove();
 
@@ -89,4 +79,12 @@ function pickComputerMove() {
   }
 
   return computerMove;
+}
+
+function resetScore() {
+  score.wins = 0;
+  score.losses = 0;
+  score.ties = 0;
+  localStorage.removeItem('score');
+  updateScoreElement();
 }
