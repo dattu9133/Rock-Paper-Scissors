f372ff8fe87923737660b12fd540fdbb2938fd14
dattu9133 <dattasaimoganti@gmail.com>
2024-05-29 15:29:34 +0530

    refactor: Add event listeners for rock, paper, and scissors buttons

--- assests/index.js
+++ assests/index.js
@@ -20,6 +20,21 @@ function autoPlay() {
   }
 }
 
+document.querySelector('.rock-button').addEventListener('click', () => playGame('rock'));
+document.querySelector('.paper-button').addEventListener('click', () => playGame('paper'));
+document.querySelector('.scissors-button').addEventListener('click', () => playGame('scissors'));
+
+
+document.body.addEventListener('keydown', (event) => {
+  if (event.key == 'r')
+    playGame('rock');
+  else if (event.key == 'p')
+    playGame('paper');
+  else if (event.key = 's') {
+    playGame('scissors');
+  }
+});
+
 function playGame(playerMove) {
   const computerMove = pickComputerMove();
 
